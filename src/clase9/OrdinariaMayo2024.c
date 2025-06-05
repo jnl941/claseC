6ae0a64217a3e0972f1b03fa67c26eed57bb52b9
Javier Navarro Lazaro <jnl941@inlumine.ual.es>
2025-06-05 20:57:51 +0200

    ordinariaMayo2024

---
+++ src/clase9/OrdinariaMayo2024.c
@@ -0,0 +1,54 @@
+/*1.- (1.5) Completar un programa que calcula e imprime las frecuencias relativas de 
+hasta 1000 tiradas de dados generadas por la funcion tirar dados, que devuelve el número de tiradas (n) 
+ejecutadas por el experimento y un vector con los valores "tiradas" 
+(esta función no debe implementarse pero sí completar su interfaz). 
+El programa calculará sobre el vector frecuencia_absoluta los conteos del vector de tiradas y, 
+puesto que se conoce el tamaño de la muestra, recorrerá el vector de frecuencia_absoluta mostrando la 
+frecuencia relativa de los valores de 1 a 6. A modo de ayuda se indica con puntos suspensivos los 
+lugares donde, como mínimo, habría que incluir código. 
+
+Ejemplo: si tenemos 10 tiradas (1,2,3,4,2,1,6,5,1}, la frecuencia absoluta de 1 es 3 y su frecuencia 
+relativa es 3/10 en el caso de 4 su frecuencia absoluta es 1 y su frecuencia relativa 4/10.*/
+
+#include <stdio.h>
+
+#define MAX_TIRADAS 1000
+
+typedef int tipo_tiradas[MAX_TIRADAS];
+typedef struct  {
+    int frecuencia_absoluta;
+    float frecuencia_relativa;
+} tipo_frecuencias[6];
+
+int tirar_dados(tipo_tiradas tiradas) {
+    for(int i = 0; i < MAX_TIRADAS; i++){
+        int numero_sacado = (int) rand() % 6 + 1;
+        tiradas[i] = numero_sacado;
+    }
+    return MAX_TIRADAS; // Retorna el número de tiradas realizadas
+}
+//n = 1000 tiradas
+//frecuencia_absoluta = int
+int main() {
+    // Definicion e inicializacion de las frecuencias
+    tipo_frecuencias frecuencias;
+    for (int i = 0; i < 6; i++) {
+        frecuencias[i].frecuencia_absoluta = 0;
+    }
+    tipo_tiradas tiradas;
+    int n = tirar_dados(tiradas); //llamada a la funcion ya construida
+    for (int i = 0; i < n; ++i) {
+        frecuencias[tiradas[i] - 1].frecuencia_absoluta++;
+    }
+
+    
+    printf("\nFrecuencias relativas:\n");
+    for(int i = 0; i < 6; i++)
+    {
+        frecuencias[i].frecuencia_relativa = (float)frecuencias[i].frecuencia_absoluta / (float)n;
+        printf("Tirada %d: %.2f\n", i + 1, (float)frecuencias[i].frecuencia_absoluta / n);
+    }
+    return 0;
+}
+
+// ubicacion de la implementacion de la funcion tirar_dados que no deben implementar
